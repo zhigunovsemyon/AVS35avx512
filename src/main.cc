87e526dd886bf631f21f75c9c7c72d07e055988f
Semyon <zhigunovsemen@gmail.com>
2025-11-09 21:13:58 +0300

    Добавил функции под fp

--- src/main.cc
+++ src/main.cc
@@ -13,10 +13,64 @@ typedef uint16_t u16;
 typedef int32_t i32;
 typedef uint32_t u32;
 
+static void avx512_f64_mult()
+{
+	using type = f64;
+	puts("Задание 1. f64 C = A * B");
+	type a[4] = {3.5, 111.0, -std::numeric_limits<type>::infinity(), -10.0};
+	type b[4] = {0.5, std::numeric_limits<type>::min(), -0.1, 12.3};
+	type c[4] = {0.0, 0.0, 0.0, 0.0};
+
+	printf("a: ");
+	print_array(std::span{a, 4});
+	printf("b: ");
+	print_array(std::span{b, 4});
+
+	auto vec_a = _mm256_load_pd(a);
+	auto vec_b = _mm256_load_pd(b);
+	auto vec_c = _mm256_mul_pd(vec_a, vec_b);
+	_mm256_store_pd(c, vec_c);
+
+	printf("c: ");
+	print_array(std::span{c, 4});
+}
+
+static void avx512_f32_div()
+{
+	using type = f32;
+	puts("Задание 2. f32 C = A / B");
+	type a[8] = {
+		1.1f,	13.37f, 3.5f, 1.0f,
+		-10.0f, 8.8f,	0.0f, std::numeric_limits<type>::quiet_NaN()};
+
+	type b[8] = {2.28f,
+		     54.6f,
+		     0.0f,
+		     99099.9f,
+		     -0.0f,
+		     std::numeric_limits<type>::max(),
+		     std::numeric_limits<type>::min(),
+		     0.5f};
+	type c[8] = {};
+
+	printf("a: ");
+	print_array(std::span{a, 8});
+	printf("b: ");
+	print_array(std::span{b, 8});
+
+	auto vec_a = _mm256_load_ps(a);
+	auto vec_b = _mm256_load_ps(b);
+	auto vec_c = _mm256_div_ps(vec_a, vec_b);
+	_mm256_store_ps(c, vec_c);
+
+	printf("c: ");
+	print_array(std::span{c, 8});
+}
+
 static void avx512_i32_sub()
 {
 	using type = i32;
-	puts("Задание 1. i32 C = A - B");
+	puts("Задание 3. i32 C = A - B");
 	type a[8] = {0, 1, std::numeric_limits<type>::min(), 11, 256, 4, 99, 0};
 	type b[8] = {1, 38, 4, 211, 0, 2, 19, std::numeric_limits<type>::max()};
 	type c[8] = {};
@@ -38,7 +92,7 @@ static void avx512_i32_sub()
 static void avx512_i8_sum()
 {
 	using type = i8;
-	puts("Задание 3. i8 C = A + B");
+	puts("Задание 5. i8 C = A + B");
 	type a[32] = "Этот текст из 32 Б.";
 	type b[32] = "This string has just 2^5 bytes.";
 	type c[32] = {};
@@ -60,7 +114,7 @@ static void avx512_i8_sum()
 static void avx512_u16_sum()
 {
 	using type = u16;
-	puts("Задание 2. u16 C = A + B");
+	puts("Задание 4. u16 C = A + B");
 	type a[16] = {0,   1,	 std::numeric_limits<type>::max(),
 		      11,  220, 4,
 		      99,  1,	 12,
@@ -88,6 +142,12 @@ static void avx512_u16_sum()
 
 int main()
 {
+	avx512_f64_mult();
+	std::println();
+
+	avx512_f32_div();
+	std::println();
+
 	avx512_i32_sub();
 	std::println();
 
